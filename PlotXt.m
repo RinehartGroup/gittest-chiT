9f87c0e1a8b39116a2db206ade58f92a0d1907df
Rinehart Group at UCSD <42098643+RinehartGroup@users.noreply.github.com>
2018-09-19 15:19:42 -0700

    Update PlotXt.m

--- PlotXt.m
+++ PlotXt.m
@@ -1,10 +1,10 @@
-T = Xtdata.TemperatureK ; ... An elipsis allows in line commenting
-H = Xtdata.MagneticFieldOe ;
-mw = 595.17 ; ... input this manually
-mass = 0.0694 ; ... input this manually
-moles = mass / mw ;
-Mag = Xtdata.DCMomentErrFreeCtremu ./ moles ;
-Xt = (Mag .* T) ./ H ;
+T = Xtdata.TemperatureK; % An elipsis allows in line commenting
+H = Xtdata.MagneticFieldOe;
+mw = 595.17; % input this manually
+mass = 0.0694; % input this manually
+moles = mass / mw;
+Mag = Xtdata.DCMomentErrFreeCtremu ./ moles;
+Xt = (Mag .* T) ./ H;
 hold on
 plot(T, Xt, 'ko')
 title('Magnetic Susceptibility')
